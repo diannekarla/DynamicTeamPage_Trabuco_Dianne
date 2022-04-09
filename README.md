e0fe82940487a0120ea513b26102f99ad420d409
Dianne <diannekarlatrabuco@gmail.com>
2022-04-09 08:38:58 -0400

    adding all files

---
+++ README.md
@@ -0,0 +1,35 @@
+![About Page](images/About Page.png "About Page")
+
+# Dynamic Team Page
+
+Design a bio page! Consider this a page in the context of a larger studio site - look at how “real”
+design / dev houses do this, and come up with your own style.
+
+# Additional Requirement
+1. Include a written plan / path with your submission. Analyze the requirements
+and provide the solution
+2. The developer will create a project repo and a README.md document for the repo with
+detailed information about the project
+3. Create a design branch; the designer will push the web assets to this branch. The
+designer is responsible for the HTML and CSS for the project
+4. Create a dev branch for each member; BOTH need to add bio information to the array
+5. Merge everything to the main branch and submit a link to the dropbox on FOL - a .txt
+file is fine.
+
+Written Plan: https://docs.google.com/document/d/1Q2-KNAVXKDvij6asXoWdLzwzl4ugnm-Uf9bujBoFizA/edit
+
+## Contributing
+
+	1. Fork it!
+	2. Create your feature branch: `git checkout -b my-new-feature`
+	3. Commit your changes: `git commits -m 'Add some feature'`
+	4. Push to the branch: `git push origin my-new-feature`
+	5. Submit a pull request :D
+
+## Credits
+
+* Prof Trev
+* Javascript Libraries
+
+## License
+Dianne Karla Trabuco
---
+++ js/main.js
@@ -0,0 +1,105 @@
+function validateAge(){
+    year = document.getElementById("year").value;
+    month = document.getElementById("month").value;
+    day = document.getElementById("day").value;
+    dob = new Date(year+" "+month+" "+day);
+
+    var today = new Date();
+    var age = today.getFullYear() - dob.getFullYear();
+    var m = today.getMonth() - dob.getMonth();
+    if (m < 0 || (m === 0 && today.getDate() < dob.getDate())) {
+        age--;
+    }
+
+    if(age >= 18){
+        window.location.href="home.html"
+    }else{
+        result = document.getElementById("result");
+        if (result.classList.contains('hidden')) {
+            setTimeout(function () {
+                result.classList.remove('hidden');
+            }, 20);
+        }
+
+    }
+
+    return false;
+}
+
+function displayFAQ() {
+    const answers = document.getElementByClass()
+    alert();
+    answers.classList.toggle("show")
+}
+
+function displayLabel(element){
+    target = element.id;
+    document.getElementById(target+"-label").classList.add("opacity-1");
+}
+
+function changeImage(element){
+    target = element.dataset.target;
+    document.getElementById("cap").src="assets/sliced-images/"+target+"/section-01.png";
+    document.getElementById("bottle").src="assets/sliced-images/"+target+"/section-02.png";
+    document.getElementById("beer").src="assets/sliced-images/"+target+"/section-03.png";
+}
+
+
+function showDesc(e){
+    var elems = document.querySelectorAll(".carousel-content-item"); //remove for all
+    [].forEach.call(elems, function(el) {
+        el.classList.remove("active");
+    });
+
+    document.getElementById(e).classList.add("active"); //add on clicked
+}
+
+
+function displayNavMobile() {
+    var x = document.getElementById("main-nav");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+}
+
+window.addEventListener('load', function(){
+
+    /* About Us Slider*/
+    new Glider(document.querySelector('.glider'), {
+        slidesToScroll: 1,
+        slidesToShow: 4.5,
+        arrows: {
+          prev: '.glider-prev',
+          next: '.glider-next'
+        },
+        dots: '.dots'
+    });
+
+
+    /* About Us Accordion*/
+    const faqItems = document.querySelectorAll('.faq-item');
+
+    faqItems.forEach(faqItem =>{
+        let btn = faqItem.querySelector('.question button');
+        let icon = faqItem.querySelector('.question button i');
+        var answer = faqItem.lastElementChild;
+        var answers = document.querySelectorAll('.faq-item .answer');
+
+        btn.addEventListener('click', ()=>{
+            answers.forEach(ans =>{
+                let ansIcon = ans.parentElement.querySelector('button i');
+                if(answer !== ans){
+                    ans.classList.add('hidden');
+                    ansIcon.className = 'arrow down';
+                }
+            });
+
+            answer.classList.toggle('hidden');
+            icon.className === 'arrow down' ? icon.className = 'arrow up'
+            : icon.className ='arrow down';
+        });
+    });
+
+});
